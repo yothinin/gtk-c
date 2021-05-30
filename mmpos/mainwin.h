0a656b4e262ff21f39e668769b2a7c21c6e1ba51
yothinin <yothinin@hotmail.com>
2021-05-30 15:21:45 +0700

    Add function.

--- mmpos/mainwin.h
+++ mmpos/mainwin.h
@@ -1,8 +1,15 @@
 #include <gtk/gtk.h>
-
-void on_shutdown(GtkWidget *widget, gpointer user_data);
+#ifndef MAINWIN_H
+#define MAINWIN_H
 gint updateclock(gpointer ptr);
+void on_shutdown(GtkWidget *widget, gpointer user_data);
 void onMenuClick(GtkWidget *widget, gpointer user_data);
 void createPopover(gpointer btn);
 void createToolbar(gpointer win);
 void activate(GtkApplication *app, gpointer user_data);
+
+void crateHomeWindow();
+void crateSaleWindow(GtkWidget *widget, gpointer win);
+void crateReportWindow(GtkWidget *widget, gpointer win);
+
+#endif
